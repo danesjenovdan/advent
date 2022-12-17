a3f15e58da8936974ee7759eb38f054c871b6b8b
mzgajner <mato@zgajner.com>
2022-12-17 15:50:16 +0100

    Solve day 14

---
+++ mato/14/part-one.test.ts
@@ -0,0 +1,11 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import solve from "./part-one.ts";
+Deno.test("solve", () => {
+  assertEquals(
+    solve([
+      "498,4 -> 498,6 -> 496,6",
+      "503,4 -> 502,4 -> 502,9 -> 494,9",
+    ]),
+    24,
+  );
+});
---
+++ mato/14/part-two.ts
@@ -0,0 +1,157 @@
+enum GridElement {
+  Air,
+  Rock,
+  Sand,
+}
+
+class Vector {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+
+  constructor([x1, y1]: [number, number], [x2, y2]: [number, number]) {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+}
+
+const parseVectors = (lines: string[]) =>
+  lines
+    .map((line) => {
+      const vectors: Vector[] = [];
+      const points = line
+        .split(" -> ")
+        .map(
+          (point: string) => point.split(",").map(Number) as [number, number],
+        );
+      for (let i = 1; i < points.length; i++) {
+        vectors.push(new Vector(points[i - 1], points[i]));
+      }
+      return vectors;
+    })
+    .flat();
+
+const normalizeVectors = (vectors: Vector[]) => {
+  let maxX = 0;
+
+  const maxY =
+    vectors.reduce((max, vector) => Math.max(max, vector.y1, vector.y2), 0) + 2;
+
+  vectors.push(new Vector([500 - maxY, maxY], [500 + maxY, maxY]));
+
+  const minX = vectors.reduce(
+    (min, vector) => Math.min(min, vector.x1, vector.x2),
+    500,
+  );
+
+  vectors.forEach((vector) => {
+    vector.x1 = vector.x1 - minX;
+    vector.x2 = vector.x2 - minX;
+
+    maxX = Math.max(maxX, vector.x1, vector.x2);
+  });
+
+  return { maxX, maxY, minX };
+};
+
+const generateGrid = (maxX: number, maxY: number) => {
+  const grid: GridElement[][] = [];
+  for (let y = 0; y <= maxY; y++) {
+    grid[y] = [];
+    for (let x = 0; x <= maxX; x++) {
+      grid[y][x] = GridElement.Air;
+    }
+  }
+  return grid;
+};
+
+const populateGridWithRock = (vectors: Vector[], grid: GridElement[][]) => {
+  vectors.forEach((vector) => {
+    if (vector.x1 === vector.x2) {
+      const bigger = Math.max(vector.y1, vector.y2);
+      const smaller = Math.min(vector.y1, vector.y2);
+      for (let y = smaller; y <= bigger; y++) {
+        grid[y][vector.x1] = GridElement.Rock;
+      }
+    } else {
+      const bigger = Math.max(vector.x1, vector.x2);
+      const smaller = Math.min(vector.x1, vector.x2);
+      for (let x = smaller; x <= bigger; x++) {
+        grid[vector.y1][x] = GridElement.Rock;
+      }
+    }
+  });
+};
+
+const addOneSand = (grid: GridElement[][], minX: number) => {
+  let sandX = 500 - minX;
+  let sandY = 0;
+
+  if (grid[sandY][sandX] === GridElement.Sand) {
+    // Pile has reached the top
+    return false;
+  }
+
+  grid[sandY][sandX] = GridElement.Sand;
+
+  while (true) {
+    if (grid[sandY + 1] === undefined) {
+      // Sand falls through the bottom
+      grid[sandY][sandX] = GridElement.Air;
+      return false;
+    } else if (grid[sandY + 1][sandX] === GridElement.Air) {
+      // Sand can move 1 down
+      grid[sandY][sandX] = GridElement.Air;
+      sandY++;
+      grid[sandY][sandX] = GridElement.Sand;
+    } else if (grid[sandY + 1][sandX - 1] === undefined) {
+      // Sand falls to the left
+      grid[sandY][sandX] = GridElement.Air;
+      return false;
+    } else if (grid[sandY + 1][sandX - 1] === GridElement.Air) {
+      // Sand can move down-left
+      grid[sandY][sandX] = GridElement.Air;
+      sandY++;
+      sandX--;
+      grid[sandY][sandX] = GridElement.Sand;
+    } else if (grid[sandY + 1][sandX + 1] === undefined) {
+      // Sand falls to the right
+      grid[sandY][sandX] = GridElement.Air;
+      return false;
+    } else if (grid[sandY + 1][sandX + 1] === GridElement.Air) {
+      // Sand can move down-right
+      grid[sandY][sandX] = GridElement.Air;
+      sandY++;
+      sandX++;
+      grid[sandY][sandX] = GridElement.Sand;
+    } else {
+      // Sand remains in place
+      return true;
+    }
+  }
+};
+
+const countSand = (grid: GridElement[][]) =>
+  grid.reduce(
+    (sum: number, row) =>
+      sum + row.filter((item) => item === GridElement.Sand).length,
+    0,
+  );
+
+export default (lines: string[]) => {
+  const vectors = parseVectors(lines);
+  const { maxX, maxY, minX } = normalizeVectors(vectors);
+  const grid = generateGrid(maxX, maxY);
+  populateGridWithRock(vectors, grid);
+
+  let keepSimulating = true;
+
+  while (keepSimulating) {
+    keepSimulating = addOneSand(grid, minX);
+  }
+
+  return countSand(grid);
+};
